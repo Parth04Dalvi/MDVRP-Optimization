de8f5e5145a4218732cc73ae65f2549025a696ab
Parth Samir Dalvi <110654561+Parth04Dalvi@users.noreply.github.com>
2026-04-05 22:11:05 -0400

    Update mst.py

--- src/mst.py
+++ src/mst.py
@@ -1,1 +1,25 @@
 
+from utils import euclidean_distance
+
+def mst_cost(nodes, dist_matrix):
+    if len(nodes) <= 1:
+        return 0
+
+    visited = set([nodes[0]])
+    cost = 0
+
+    while len(visited) < len(nodes):
+        min_edge = float('inf')
+        next_node = None
+
+        for u in visited:
+            for v in nodes:
+                if v not in visited:
+                    if dist_matrix[u][v] < min_edge:
+                        min_edge = dist_matrix[u][v]
+                        next_node = v
+
+        cost += min_edge
+        visited.add(next_node)
+
+    return cost
